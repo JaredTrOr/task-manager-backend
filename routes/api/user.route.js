268807cf8d6329ac00b2c93dbd6cad61a0c3ac90
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-15 00:47:53 -0600

    add login route

--- routes/api/user.route.js
+++ routes/api/user.route.js
@@ -1,11 +1,8 @@
 import { Router } from "express"
+import UserController from "../../controllers/user.controller.js";
 const userRouter = Router()
 
-userRouter.post('/signup', (req, res) => {
-    
-
-
-
-})
+userRouter.post('/signup', UserController.signup)
+userRouter.post('/login', UserController.login)
 
 export default userRouter;
