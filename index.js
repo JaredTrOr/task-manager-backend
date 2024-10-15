2309d1f5af9553eb5c5e9f03128f60c8530e30cf
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-15 00:45:34 -0600

    Implementación de cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import router from './routes/router.js'
 import { connectToMongoDB } from './db/connection.js'
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT
 const app = express()
 connectToMongoDB()
 
+app.use(cors())
 app.use(express.json())
 app.use('/', router)
 app.listen(PORT, () => {
