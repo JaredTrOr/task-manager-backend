2cd413efee4f0c7f24223c9c35ff3ff8cf0dbcea
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-22 19:14:26 -0600

    Creación de middleware para validación de token

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken'
+
+async function isAuth (req, res, next) {
+
+    try {
+        if (!req.headers.authorization) {
+            return res.status(401).json({ success: false, message: 'Unauthorized request' })
+        }
+    
+        const token = req.headers.authorization.split(' ')[1]
+    
+        if (token === 'null') {
+            return res.status(401).json({ success: false, message: 'Unauthorized request' })
+        }
+    
+        const payload = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        if (!payload) {
+            return res.status(401).json({ success: false, message: 'Unauthorized request' })
+        }
+    
+        req._id = payload._id
+        next()
+    } catch(err) {
+        console.log(err)
+        return res.status(401).json({ success: false, message: 'Unauthorized request' })
+    }
+    
+}
+
+export default isAuth
--- routes/router.js
+++ routes/router.js
@@ -2,12 +2,13 @@ import { Router } from "express"
 import userRouter from "./api/user.route.js"
 import taskRouter from "./api/task.route.js"
 import listTypeRouter from "./api/list-type.route.js"
+import isAuth from "../middlewares/verifyToken.js"
 
 const router = Router()
 
 router.get('/', (req,res) => res.json({message: 'Task manager sever running :3'}))
 router.use('/user', userRouter)
-router.use('/task', taskRouter)
-router.use('/list-type', listTypeRouter)
+router.use('/task',isAuth, taskRouter)
+router.use('/list-type',isAuth, listTypeRouter)
 
 export default router;
