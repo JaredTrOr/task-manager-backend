d9542d9dca7950aa27301a974f2a265ceb93a908
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-27 22:45:34 -0600

    Agregación de rutas para obtener el perfil del usuario asi como actualizarlo, implementación de controladores

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -17,7 +17,6 @@ class UserController {
                 return res.json({ success: false, message: 'El usuario ya existe, por favor de ingresar uno nuevo'})
 
             const password = req.body.password
-            console.log()
             const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_ROUNDS))
             req.body.password = hashedPassword
 
@@ -31,7 +30,7 @@ class UserController {
             const token = jwt.sign(tokenPayload, process.env.JWT_SECRET_KEY)
             res.status(200).json({
                 success: true, 
-                message: 'User created successfully',
+                message: 'Usuario creado exitosamente',
                 data: {
                     user: newUser,
                     token
@@ -52,7 +51,7 @@ class UserController {
 
             const user = await UserModel.findOne({ email })
             if (!user) 
-                return res.json({ success: false, message: 'Email ya existente' })
+                return res.json({ success: false, message: 'Email no existe' })
 
             const isValidPassword = bcrypt.compare(password, user.password)
             if (!isValidPassword) 
@@ -68,10 +67,63 @@ class UserController {
                 success: true, 
                 message: 'Login successfully',
                 data: {
-                    user,
                     token
                 }
             })
+        } catch(err) {
+            res.json({
+                success: false, 
+                message: `error: ${err}`,
+            })
+            console.error(err)
+        }
+    }
+
+    // Functions that requires authentication
+    static async profile(req, res) {
+        try {
+            const user = await UserModel.findById(req._id)
+            if (!user) 
+                return res.json({ success: false, message: 'Usuario no encontrado' })
+
+            res.json({
+                success: true, 
+                message: 'El perfil se ha obtenido exitosamente',
+                data: { 
+                    _id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    username: user.username,
+                    image: user.image
+                }
+            })
+        } catch(err) {
+            res.json({
+                success: false, 
+                message: `error: ${err}`,
+            })
+            console.error(err)
+        }
+    }
+
+    static async updateProfile(req, res) {
+        try  {
+            const userId = req._id
+
+            const existingUsername = await UserModel.findOne({ username: req.body.username, _id: { $ne: userId } })
+            if (existingUsername) return res.json({ success: false, message: 'El username ya existe, por favor de ingresar uno nuevo' })
+
+            const existingEmail = await UserModel.findOne({ email: req.body.email, _id: { $ne: userId } })
+            if (existingEmail) return res.json({ success: false, message: 'El email ya existe, por favor de ingresar uno nuevo' })
+
+            const updatedUser =  await UserModel.findByIdAndUpdate(userId, req.body, { new: true })
+
+            res.json({
+                success: true, 
+                message: 'Perfil actualizado exitosamente',
+                data: updatedUser
+            }) 
+
         } catch(err) {
             res.json({
                 success: false, 
--- routes/api/user.route.js
+++ routes/api/user.route.js
@@ -1,8 +1,11 @@
 import { Router } from "express"
-import UserController from "../../controllers/user.controller.js";
+import UserController from "../../controllers/user.controller.js"
+import isAuth from "../../middlewares/verifyToken.js"
 const userRouter = Router()
 
 userRouter.post('/signup', UserController.signup)
 userRouter.post('/login', UserController.login)
+userRouter.get('/get-profile', isAuth ,UserController.profile)
+userRouter.put('/update-profile', isAuth, UserController.updateProfile)
 
 export default userRouter;
