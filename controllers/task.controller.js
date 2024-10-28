bfcc3d1c8a914a726a025429184998d4ee8f13f4
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-10-27 22:46:07 -0600

    Creación de rutas para CRUD de tareas

---
+++ controllers/task.controller.js
@@ -0,0 +1,96 @@
+import TaskModel from "../models/TaskModel.js";
+
+class TaskController {
+
+    static async getTasks (req,res) {
+        try {
+            const userId = req._id
+            const tasks = await TaskModel.find({ creator: userId })
+            res.json({
+                success: true,
+                data: tasks
+            })
+        } catch(err) {
+            res.json({
+                success: false,
+                message: `error: ${err}`
+            })
+            console.error(err)
+        }
+    }
+
+    static async createTask (req,res) {
+        try {
+
+            req.body.creator = req._id
+            const newTask = new TaskModel(req.body)
+            await newTask.save()
+            res.json({
+                success: true,
+                message: 'La tarea se ha creado exitosamente',
+                data: newTask
+            })
+
+        } catch(err) {
+            res.json({
+                success: false,
+                message: `error: ${err}`
+            })
+            console.error(err);
+        }
+    }
+
+    static async updateTask(req, res) {
+        try {
+            const updatedTask = await TaskModel.findByIdAndUpdate(req.body._id, req.body)
+            res.json({
+                success: true,
+                message: 'La tarea se ha actualizado exitosamente',
+                data: updatedTask
+            })
+        } catch(err) {
+            res.json({
+                success: false,
+                message: `error: ${err}`
+            })
+            console.error(err)
+        }
+    }
+
+    static async deleteTask(req, res) {
+        try {
+            const deletedTask = await TaskModel.deleteOne({ _id: req.params.id })
+            res.json({
+                success: true,
+                message: 'La tarea fue eliminada exitosamente',
+                data: deletedTask
+            })
+
+        } catch(err) {
+            res.json({
+                success: false,
+                message: `error: ${err}`
+            })
+            console.error(err)
+        }
+    }
+
+    static async deleteCompletedTasks(req, res) {
+        try {
+            await TaskModel.deleteMany({ checked: true })
+            res.json({
+                success: true,
+                message: 'Se han eliminado las tareas completadas',
+            })
+        } catch(err) {
+            res.json({
+                success: false,
+                message: `error: ${err}`
+            })
+            console.error(err)
+        }
+    }
+
+}
+
+export default TaskController
--- routes/api/task.route.js
+++ routes/api/task.route.js
@@ -1,4 +1,12 @@
 import { Router } from "express"
+import TaskController from "../../controllers/task.controller.js"
+
 const taskRouter = Router()
+
+taskRouter.get('/get-tasks', TaskController.getTasks)
+taskRouter.post('/create-task', TaskController.createTask)
+taskRouter.put('/update-task', TaskController.updateTask)
+taskRouter.delete('/delete-task/:id', TaskController.deleteTask)
+taskRouter.delete('/delete-completed-tasks', TaskController.deleteCompletedTasks)
 
 export default taskRouter;
